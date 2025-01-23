6ba86f8a0b524b53e975c08763133e85da1de4d1
MetinBiter <145712932+Teyon-Tet@users.noreply.github.com>
2025-01-23 23:22:14 +0300

    file update

--- Assets/Scripts/Runtime/Data/UnityObjects/CD_Input.cs
+++ Assets/Scripts/Runtime/Data/UnityObjects/CD_Input.cs
@@ -1,7 +1,7 @@
-﻿using Data.ValueObjects;
+﻿using Runtime.Data.ValueObjects;
 using UnityEngine;
 
-namespace Data.UnityObjects
+namespace Runtime.Data.UnityObjects
 {
     [CreateAssetMenu(fileName = "CD_Input", menuName = "3DGame/CD_Input", order = 0)]
     public class CD_Input : ScriptableObject
--- Assets/Scripts/Runtime/Data/UnityObjects/CD_Level.cs
+++ Assets/Scripts/Runtime/Data/UnityObjects/CD_Level.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
-using Data.ValueObjects;
+using Runtime.Data.ValueObjects;
 using UnityEngine;
 
-namespace Data.UnityObjects
+namespace Runtime.Data.UnityObjects
 {
     [CreateAssetMenu(fileName = "CD_Level", menuName = "3DGame/CD_level", order = 0)]
     public class CD_Level : ScriptableObject
